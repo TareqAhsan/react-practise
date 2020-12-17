c5cd89bae1d59ee0f2c7a509fb45bb2dfcba3612
Tareq <tareqahsan61@gmail.com>
2020-12-17 16:51:09 +0600

    commit stop watch

--- src/App.js
+++ src/App.js
@@ -3,23 +3,71 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react'
 import Profile from './compoonent/profile/Profile'
-
+import Bio from './compoonent/profile/Bio'
+import Skill from './compoonent/profile/Skill';
 // import Counter from './compoonent/Counter'
 
 
 class App extends Component{
-
+  state = {
+    count: 4
+ }
   
+  intervalID = null;
 
 
-  
-  
+  decrease = () => {
+    if (this.state.count > 0) {
+      this.setState({ count: this.state.count - 1 })
+    }
+  }
+  increase = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+  setTimer = () => {
+    if (this.state.count > 0 && !this.intervalID) {
+     this.intervalID= setInterval(() => {
+        this.setState({ count: this.state.count - 1 }, () => {
+          if (this.state.count === 0) {
+            alert('Timer finished')
+            clearInterval(this.intervalID)
+            this.intervalID = null;
+          }
+        });
+      },1000)
+    }
+  }
+
+  stopTimer = () => {
+    if (this.intervalID) {
+      clearInterval(this.intervalID)
+      this.intervalID =null
+    }
+  }
+
+  resettimer = () => {
+    this.setState({ count: 0 });
+    clearInterval(this.intervalID)
+    this.intervalID=null
+  }
   render() {
   
     return (
       <div className="App">
-         <Profile></Profile>
-      </div>
+        <h1 className='Heading'>Simple timer</h1>
+        <div className='container'>
+          <button className='btn' onClick={this.decrease}>-</button>
+          <span>{ this.state.count}</span>
+          <button className='btn' onClick={this.increase}>+</button>
+        </div>
+        <div className="container">
+          <button className='btn' onClick= {this.setTimer }>start</button>
+          <button className='btn'onClick ={this.stopTimer}>stop</button>
+          <button className='btn' onClick={this.resettimer}>reset</button>
+          
+        </div>
+           
+        </div>
     );
   
   }
